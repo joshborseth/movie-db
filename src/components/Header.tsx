0f9ff8da1c0f6f653642e1b44eb173e12b11882a
Joshua Borseth <joshuaborseth@gmail.com>
2022-08-19 16:43:16 -0700

    styling and using our user authenticated data

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <nav className="h-20">
+      <ul className="flex h-full w-full items-center justify-center"></ul>
+    </nav>
+  );
+};
+
+export default Header;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,30 +5,20 @@ import CredentialsProvider from "next-auth/providers/credentials";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
-
+if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET)
+  throw new Error("env variables not defined");
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
+  theme: {
+    colorScheme: "light",
+  },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
     // ...add more providers here
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        name: {
-          label: "Name",
-          type: "text",
-          placeholder: "Enter your name",
-        },
-      },
-      async authorize(credentials, _req) {
-        const user = { id: 1, name: credentials?.name ?? "J Smith" };
-        return user;
-      },
-    }),
   ],
 };
 
